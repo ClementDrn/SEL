3f28193cdbda14ac1f18d49e1bced85784ae2789
Clément Darne <clementdarne8888@gmail.com>
2022-03-27 23:14:24 -0400

    #4 Fixed mistake concerning include paths in Vec.hpp.

--- include/SEL/Maths/Vec.hpp
+++ include/SEL/Maths/Vec.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-#include "SEL/Vecs/Vec2.hpp"
-#include "SEL/Vecs/Vec3.hpp"
-#include "SEL/Vecs/Vec4.hpp"
+#include "SEL/Maths/Vecs/Vec2.hpp"
+#include "SEL/Maths/Vecs/Vec3.hpp"
+#include "SEL/Maths/Vecs/Vec4.hpp"
