f09c478247ffa321473d93fd2c1aa5b2a0d53b68
Clément Darne <clementdarne8888@gmail.com>
2022-03-30 21:42:48 -0400

    #5 Added documentation comments for matrices and added constexpr for their [] overload.

--- include/SEL/Maths/Matrices/Mat3x4.hpp
+++ include/SEL/Maths/Matrices/Mat3x4.hpp
@@ -2,18 +2,44 @@
 
 namespace sel {
 
+	/// @brief Representation of a 3x4 matrix.
+	/// 
+	/// @tparam T is the type of the matrix's values.
+	///  
 	template <typename T>
 	class Mat3x4
 	{
 	public:
 
+		/// @brief Default constructor.
+		///
+		/// This constructor sets matrix's values to default.
+		/// 
 		Mat3x4() = default;
 
 
-		T* operator[](size_t idx) { return m_data[idx]; }
-		const T* operator[](size_t idx) const { return m_data[idx]; }
+		/// @brief Access specified matrix column.
+		/// 
+		/// Returns the column at specified location index (idx). No bounds checking is performed.
+		/// To access a specific value of the matrix, first retrieve a column, then access to one of its values 
+		/// by providing another index, you will end up with a two coordinate access.
+		/// 
+		/// @param idx is the index of the matrix column to retrieve.
+		/// 
+		/// @return The specified matrix column.
+		/// 
+		constexpr T* operator[](size_t idx) { return m_data[idx]; }
+		constexpr const T* operator[](size_t idx) const { return m_data[idx]; }
 
 
+		/// @brief Overload of += binary arithmetic operator.
+		/// 
+		/// The other matrix is added to the instance.
+		/// 
+		/// @param rhs is the matrix which must be added to the instance.
+		/// 
+		/// @return The reference to the updated matrix.
+		/// 
 		Mat3x4& operator+=(const Mat3x4& rhs)
 		{
 			m_data[0][0] += rhs[0][0];
@@ -34,6 +60,14 @@ namespace sel {
 			return *this;
 		}
 
+		/// @brief Overload of -= binary arithmetic operator.
+		/// 
+		/// The instance is substracted by the other matrix.
+		/// 
+		/// @param rhs is the matrix which must be substract the instance.
+		/// 
+		/// @return The reference to the updated matrix.
+		/// 
 		Mat3x4& operator-=(const Mat3x4& rhs)
 		{
 			m_data[0][0] -= rhs[0][0];
@@ -60,11 +94,27 @@ namespace sel {
 		T m_data[3][4] = {};
 	};
 
+	/// @brief Representation of a 3x4 integer matrix.
+	///
 	using Mat3x4i = Mat3x4<int>;
+
+	/// @brief Representation of a 3x4 float matrix.
+	///
 	using Mat3x4f = Mat3x4<float>;
+
+	/// @brief Representation of a 3x4 unsigned integer matrix.
+	///
 	using Mat3x4u = Mat3x4<unsigned int>;
 
 
+	/// @brief Overload of + binary arithmetic operator.
+	/// 
+	/// @tparam T is the type of the matrices' values.
+	/// @param lhs is the first matrix.
+	/// @param rhs is the second matrix.
+	/// 
+	/// @return The matrix resulted by the addition of the two provided matrices.
+	/// 
 	template <class T>
 	inline Mat3x4<T> operator+(Mat3x4<T> lhs, const Mat3x4<T>& rhs)
 	{
@@ -72,6 +122,14 @@ namespace sel {
 		return lhs;
 	}
 
+	/// @brief Overload of - binary arithmetic operator.
+	/// 
+	/// @tparam T is the type of the matrices' values.
+	/// @param lhs is the first matrix.
+	/// @param rhs is the second matrix.
+	/// 
+	/// @return The matrix resulted by the substraction of the two provided matrices.
+	/// 
 	template <class T>
 	inline Mat3x4<T> operator-(Mat3x4<T> lhs, const Mat3x4<T>& rhs)
 	{
--- include/SEL/Maths/Matrices/Mat4x4.hpp
+++ include/SEL/Maths/Matrices/Mat4x4.hpp
@@ -3,18 +3,44 @@
 
 namespace sel {
 
+	/// @brief Representation of a 4x4 matrix.
+	/// 
+	/// @tparam T is the type of the matrix's values.
+	///  
 	template <typename T>
 	class Mat4x4
 	{
 	public:
 
+		/// @brief Default constructor.
+		///
+		/// This constructor sets matrix's values to default.
+		/// 
 		Mat4x4() = default;
 
 
-		T* operator[](size_t idx) { return m_data[idx]; }
-		const T* operator[](size_t idx) const { return m_data[idx]; }
+		/// @brief Access specified matrix column.
+		/// 
+		/// Returns the column at specified location index (idx). No bounds checking is performed.
+		/// To access a specific value of the matrix, first retrieve a column, then access to one of its values 
+		/// by providing another index, you will end up with a two coordinate access.
+		/// 
+		/// @param idx is the index of the matrix column to retrieve.
+		/// 
+		/// @return The specified matrix column.
+		/// 
+		constexpr T* operator[](size_t idx) { return m_data[idx]; }
+		constexpr const T* operator[](size_t idx) const { return m_data[idx]; }
 
 
+		/// @brief Overload of += binary arithmetic operator.
+		/// 
+		/// The other matrix is added to the instance.
+		/// 
+		/// @param rhs is the matrix which must be added to the instance.
+		/// 
+		/// @return The reference to the updated matrix.
+		/// 
 		Mat4x4& operator+=(const Mat4x4& rhs)
 		{
 			m_data[0][0] += rhs[0][0];
@@ -40,6 +66,14 @@ namespace sel {
 			return *this;
 		}
 
+		/// @brief Overload of -= binary arithmetic operator.
+		/// 
+		/// The instance is substracted by the other matrix.
+		/// 
+		/// @param rhs is the matrix which must be substract the instance.
+		/// 
+		/// @return The reference to the updated matrix.
+		/// 
 		Mat4x4& operator-=(const Mat4x4& rhs)
 		{
 			m_data[0][0] -= rhs[0][0];
@@ -71,11 +105,27 @@ namespace sel {
 		T m_data[4][4] = {};
 	};
 
+	/// @brief Representation of a 4x4 integer matrix.
+	///
 	using Mat4x4i = Mat4x4<int>;
+
+	/// @brief Representation of a 4x4 float matrix.
+	///
 	using Mat4x4f = Mat4x4<float>;
+
+	/// @brief Representation of a 4x4 unsigned integer matrix.
+	///
 	using Mat4x4u = Mat4x4<unsigned int>;
 
 
+	/// @brief Overload of + binary arithmetic operator.
+	/// 
+	/// @tparam T is the type of the matrices' values.
+	/// @param lhs is the first matrix.
+	/// @param rhs is the second matrix.
+	/// 
+	/// @return The matrix resulted by the addition of the two provided matrices.
+	/// 
 	template <class T>
 	inline Mat4x4<T> operator+(Mat4x4<T> lhs, const Mat4x4<T>& rhs)
 	{
@@ -83,6 +133,14 @@ namespace sel {
 		return lhs;
 	}
 
+	/// @brief Overload of - binary arithmetic operator.
+	/// 
+	/// @tparam T is the type of the matrices' values.
+	/// @param lhs is the first matrix.
+	/// @param rhs is the second matrix.
+	/// 
+	/// @return The matrix resulted by the substraction of the two provided matrices.
+	/// 
 	template <class T>
 	inline Mat4x4<T> operator-(Mat4x4<T> lhs, const Mat4x4<T>& rhs)
 	{
